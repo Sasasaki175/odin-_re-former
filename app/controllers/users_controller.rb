c5bd60eb2a36aa9d44c50d3384f88a57f3865356
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-06-12 15:04:11 +0900

    feat: add create action with strong parameters to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
   
   def create
+    # @user = User.new(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:name, :email, :password])
   end
 end
